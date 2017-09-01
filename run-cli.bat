d138b3923010c1bfd0fe1ff12158467656411995
ouyafei <ouyafei@pxsj.com>
2017-09-01 10:21:20 +0800

    提交前备份

--- run-cli.bat
+++ run-cli.bat
@@ -1,10 +1,10 @@
-docker build  -t php-swoole-distributed:1.2 .
+docker build -f Dockerfile-cli -t php-swoole-distributed:cli-1.2 .
 docker rm -f php-swoole-distributed-run-01
 
 docker ps -a
 docker run ^
 --name php-swoole-distributed-run-01 ^
--p 8081:8081 ^
+-p 8085:8081 ^
 -v E:\develop\code\php\swooledistributed:/app/sd ^
--t -i php-swoole-distributed:1.2 /bin/bash 
+-t -i php-swoole-distributed:cli-1.2 /bin/bash 
 
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-docker build -t php-swoole-distributed:1.1 .
+docker build -t php-swoole-distributed:1.2 .
 docker rm -f php-swoole-distributed-run-01
 docker ps -a
 
@@ -9,6 +9,6 @@ docker run \
 --name php-swoole-distributed-run-01 \
 -p 8081:8081 \
 -v E:\develop\code\php\swooledistributed:/app/sd \
--t -i php-swoole-distributed:1.1 /bin/bash 
+-t -i php-swoole-distributed:1.2 /bin/bash 
 
 
